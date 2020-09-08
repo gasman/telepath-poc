cbec7182387f5237a2533a8c89440f323b51939a
Matt Westcott <matt@west.co.tt>
2020-09-08 15:27:47 +0100

    Add jquery and move getting/setting state to return value of render

--- formfields/static/formfields/js/widget.js
+++ formfields/static/formfields/js/widget.js
@@ -4,12 +4,21 @@ class Widget {
         this.idForLabel = idForLabel;
     }
 
-    render(container, name, id, value) {
-        var div = document.createElement('div');
-        container.appendChild(div);
+    render(container, name, id) {
         var html = this.html.replace('__NAME__', name).replace('__ID__', id);
-        div.innerHTML = html;
-        document.getElementById(id).value = value;
+        var dom = $(html);
+        $(container).append(dom);
+        return {
+            getValue: function() {
+                return document.getElementById(id).value;
+            },
+            getState: function() {
+                return document.getElementById(id).value;
+            },
+            setState: function(state) {
+                document.getElementById(id).value = state;
+            },
+        }
     }
 }
 telepath.register('formfields.Widget', Widget);
