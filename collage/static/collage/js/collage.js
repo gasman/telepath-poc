c087459d0066910f1a34895759d20aa0672abe05
Matt Westcott <matt@west.co.tt>
2020-09-09 01:34:00 +0100

    simplify collage example

--- collage/static/collage/js/collage.js
+++ collage/static/collage/js/collage.js
@@ -1,16 +1,15 @@
 document.addEventListener('DOMContentLoaded', event => {
-    Array.from(document.getElementsByClassName('collage')).forEach(element => {
-        var ctx = element.getContext('2d');
-        var shapes = JSON.parse(element.dataset.shapes).map((shapeData) => {
-            return telepath.unpack(shapeData);
-        });
-
-        setInterval(() => {
-            var shape = shapes[Math.floor(Math.random() * shapes.length)];
-            shape.draw(
-                ctx,
-                Math.random() * element.width, Math.random() * element.height
-            )
-        }, 100);
+    canvas = document.getElementById('collage');
+    var ctx = canvas.getContext('2d');
+    var shapes = JSON.parse(canvas.dataset.shapes).map((shapeData) => {
+        return telepath.unpack(shapeData);
     });
+
+    setInterval(() => {
+        var shape = shapes[Math.floor(Math.random() * shapes.length)];
+        shape.draw(
+            ctx,
+            Math.random() * canvas.width, Math.random() * canvas.height
+        )
+    }, 100);
 });
--- collage/views.py
+++ collage/views.py
@@ -1,14 +1,24 @@
+import json
 from django.shortcuts import render
+from telepath import JSContext
 
-from .models import Collage
 from .shapes import Circle, Rectangle
 
+
 def index(request):
-    collage = Collage([
+    shapes = [
         Circle(30, 'red'),
         Circle(50, 'blue'),
         Rectangle(100, 50, 'yellow'),
+    ]
+
+    js_context = JSContext()
+    shapes_json = json.dumps([
+        js_context.pack(shape)
+        for shape in shapes
     ])
+
     return render(request, 'collage/index.html', {
-        'collage': collage,
+        'shapes_json': shapes_json,
+        'media': js_context.media,
     })
