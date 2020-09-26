a3b9ec89ca3beb0371534101c099136a9399e585
Matt Westcott <matt@west.co.tt>
2020-09-27 00:57:35 +0100

    add PageChooserPanel to HomePage for illustration

--- formfields/models.py
+++ formfields/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
+from wagtail.admin.edit_handlers import PageChooserPanel
 from wagtail.core.models import Page
 
 
 class PersonPage(Page):
-    pass
+    other_page = models.ForeignKey(Page, blank=True, null=True, on_delete=models.SET_NULL, related_name='+')
+
+    content_panels = Page.content_panels + [
+        PageChooserPanel('other_page'),
+    ]
